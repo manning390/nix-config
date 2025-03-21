cc73b93d5be4cc2269fab5382517e8487e2d36db
Michael Manning <michael@manning390.com>
2025-03-21 00:46:13 -0700

    Fix firefox and abidan

--- modules/abidan-archive-backup.nix
+++ modules/abidan-archive-backup.nix
@@ -11,6 +11,11 @@ in {
   options.custom.${serviceName}.enable = lib.mkEnableOption "Enables a user service that backs up the mysql database on the abidan archive";
 
   config = lib.mkIf config.custom.${serviceName}.enable {
+
+    sops.secrets.abidan_db = {
+      owner = username;
+    };
+
     systemd.user.services.${serviceName} = {
       enable = true;
       after = ["network.target" "sops-nix.service"];
