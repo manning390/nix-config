297359252969e039b3dcb8a920d38229c4efa163
Michael Manning <michael@manning390.com>
2026-01-25 19:37:01 -0800

    Try to add keybindings to foot

--- home/core/foot/default.nix
+++ home/core/foot/default.nix
@@ -6,10 +6,12 @@
   programs.foot = {
     enable = true;
     settings = {
+      # "kitty-keyboard-protocol" = "yes"; # Suppose to turn on the kitty-keyboard-protocol for keybindings
+      # These don't work at the moment, figure out what's wrong. 
       key-bindings = {
-        "Control+m" = "send-text:\x1b\x5b109;133u";
-        "Control+i" = "send-text:\x1b\x5b105;133u";
-        "Control+Shift+h" = "send-text:\x1b\x5b104;134u";
+        "Control+Shift+H" = "send-text:\\x1b\\x5b104;134u";
+        "Control+I" = "send-text:\\x1b\\x5b105;133u";
+        "Control+M" = "send-text:\\x1b\\x5b109;133u";
       };
     };
   };
