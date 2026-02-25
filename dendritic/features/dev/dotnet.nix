197fceba701ec232904220f906876dcdf4e931df
Michael Manning <michael.manning@sage.com>
2026-02-25 11:03:46 -0800

    donet devshell

---
+++ dendritic/features/dev/dotnet.nix
@@ -0,0 +1,20 @@
+{self,...}: let
+  name = "dotnet";
+in {
+  perSystem = {pkgs, ...}: {
+    devShells = {
+      ${name} = pkgs.mkShell {
+        inherit name;
+        packages = with pkgs; [
+          dotnet-sdk
+          dotnet-sdk_10
+          dotnet-ef
+          # self.packages.${pkgs.stdenv.hostPlatform.system}.easy-dotnet-server
+        ];
+        shellHook = ''
+          echo "Oh, .Net eh? Nice nice..."
+        '';
+      };
+    };
+  };
+}
--- dendritic/hosts/wsl/sage/default.nix
+++ dendritic/hosts/wsl/sage/default.nix
@@ -1,73 +1,77 @@
-{config,...}: let
-    hostname = "sage";
-    user = config.local.identity.username;
+{self, config, ...}: let
+  hostname = "sage";
+  user = config.local.identity.username;
 in {
-    local.hosts.${hostname} = {
-        type = "wsl";
-        stateVersion = "25.11";
-    };
-    flake.aspects = {aspects,...}: {
-        ${hostname} = {
-            includes = with aspects; [
-                base 
-                (homeManager._.users user)
-                docker
-            ];
+  local.hosts.${hostname} = {
+    type = "wsl";
+    stateVersion = "25.11";
+  };
+  flake.aspects = {aspects, ...}: {
+    ${hostname} = {
+      includes = with aspects; [
+        base
+        (homeManager._.users user)
+        docker
+      ];
 
-            nixos = {config,...}: {
-                imports = [
-                    ../../../../modules/common.nix
-                    ../../../../modules/shells.nix
-                ];
+      nixos = {
+        config,
+        pkgs,
+        ...
+      }: {
+        imports = [
+          ../../../../modules/common.nix
+          ../../../../modules/shells.nix
+        ];
 
-                local = {
-                    shells = {
-                        systemShell = "fish";
-                        userShell = "fish";
-                    };
-                    git.includeFile = config.sops.templates."gitconfig".path;
-                };
-
-                environment.sessionVariables = {
-                    COLEMAK = "1";
-                    NIXCONFIG = "/home/${user}/Code/nix/nix-config";
-                };
-
-                environment.shellAliases = {
-                    whostname = "echo 'AP1H85254WLR' | clip.exe";
-                };
-                sops = let
-                    sopsFile = ./secrets/sage.yaml;
-                    owner = user;
-                    group = "users";
-                    mode = "0600";
-                in {
-                    secrets = {
-                        "npm/npmrc" = {
-                            inherit sopsFile owner group mode;
-                            path = "/home/${user}/.npmrc";
-                        };
-                        "workemail" = { inherit sopsFile owner group mode;};
-                        "userProfile" = {
-                            inherit sopsFile owner group mode;
-                            path = "/home/${user}/.profile";
-                        };
-                    };
-                    templates."gitconfig"= {
-                        inherit owner group mode;
-                        content = ''
-                            [user]
-                                email = ${config.sops.placeholder.workemail}
-                        '';
-                    };
-                };
-            };
-
-            homeManager = {
-                imports = [
-                    ./_daily_logging.nix
-                ];
-            };
+        local = {
+          shells = {
+            systemShell = "fish";
+            userShell = "fish";
+          };
+          git.includeFile = config.sops.templates."gitconfig".path;
         };
+
+        environment.sessionVariables = {
+          COLEMAK = "1";
+          NIXCONFIG = "/home/${user}/Code/nix/nix-config";
+        };
+
+        environment.shellAliases = {
+          whostname = "echo 'AP1H85254WLR' | clip.exe";
+        };
+        sops = let
+          sopsFile = ./secrets/sage.yaml;
+          owner = user;
+          group = "users";
+          mode = "0600";
+        in {
+          secrets = {
+            "npm/npmrc" = {
+              inherit sopsFile owner group mode;
+              path = "/home/${user}/.npmrc";
+            };
+            "workemail" = {inherit sopsFile owner group mode;};
+            "userProfile" = {
+              inherit sopsFile owner group mode;
+              path = "/home/${user}/.profile";
+            };
+          };
+          templates."gitconfig" = {
+            inherit owner group mode;
+            content = ''
+              [user]
+                  email = ${config.sops.placeholder.workemail}
+            '';
+          };
+        };
+      };
+
+      homeManager = {
+        imports = [
+          ./_daily_logging.nix
+        ];
+      };
     };
+  };
 }
