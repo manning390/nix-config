1152d9ef0dd5f2fee36b111a3bf14866cd80fc92
Michael Manning <michael.manning@sage.com>
2026-02-26 14:33:53 -0800

    Created dotnet dev shell

--- dendritic/features/dev/dotnet.nix
+++ dendritic/features/dev/dotnet.nix
@@ -1,15 +1,15 @@
 {self,...}: let
   name = "dotnet";
 in {
-  perSystem = {pkgs, ...}: {
+  perSystem = {config, pkgs, ...}: {
     devShells = {
       ${name} = pkgs.mkShell {
         inherit name;
         packages = with pkgs; [
-          dotnet-sdk
+          # dotnet-sdk
           dotnet-sdk_10
           dotnet-ef
-          # self.packages.${pkgs.stdenv.hostPlatform.system}.easy-dotnet-server
+          self.packages.${pkgs.stdenv.hostPlatform.system}.easy-dotnet-server
         ];
         shellHook = ''
           echo "Oh, .Net eh? Nice nice..."
