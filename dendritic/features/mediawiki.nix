1c7d21da23d1f9be44c34d7a04c758d527b7aa96
Michael Manning <michael@manning390.com>
2026-04-11 17:01:34 -0700

    Trying to make progress on wiki

--- dendritic/features/mediawiki.nix
+++ dendritic/features/mediawiki.nix
@@ -1,20 +1,19 @@
 {
   flake.modules.nixos.mediawiki = {pkgs,...}: {
-    services.httpd.enable = true;
+    services.nginx = {
+
+    };
     services.mediawiki = {
       enable = true;
-      name = " Test Abidan";
-      url = "http://localhost";
-      passwordFile = pkgs.writeText "password" "password";
-      uploadsDir = "/var/lib/mediawiki/uploads";
-      httpd.virtualHost = {
-        hostName = "localhost";
-        adminAddr = "admin@example.com";
-      };
+      name = "Test Abidan";
+      url = "http://wiki.localhost";
+      passwordFile = pkgs.writeText "password" "cardbotnine";
+      webserver = "nginx";
       database = {
-        type = "mysql";
+        type = "postgres";
         createLocally = true;
       };
+      nginx.hostName = "wiki.localhost";
       poolConfig = {
         "pm" = "dynamic";
         "pm.max_children" = 10;
@@ -22,11 +21,56 @@
         "pm.min_spare_servers" = 1;
         "pm.max_spare_servers" = 3;
       };
-      extraConfig = ''
-        wfLoadExtension("Scribunto");
+      extraConfig = /* php */ ''
+        // Enable Darkmode feature!
+        $wgVectorNightMode['beta'] = true;
+        $wgVectorNightMode['logged_out'] = true;
+        $wgVectorNightMode['logged_in'] = true;
+        $wgDefaultUserOptions['vector-theme'] = 'os';
+
+        // Scribunto configs
         $wgScribuntoDefaultEngine = 'luastandalone';
         $wgScribuntoEngineConf['luastandalone']['memoryLimit'] = "50M";
+
+        // Pluggable Auth
+        $wgPluggableAuth_Config = [
+            'Abidan Archive' => [
+                'plugin' => 'OpenIDConnect',
+                'data' => [
+                    'providerURL' => 'http://localhost:8000',
+                    'clientID' => 'mediawiki',
+                    'clientsecret' => 'secret',
+                ]
+            ]
+        ];
+        // Force OAuth login
+        $wgPluggableAuth_EnableAutoLogin = false;
+        $wgPluggableAuth_EnableLocalLogin = true; // No mediawiki based accounts
+        $wgPluggableAuth_EnableLocalProperties = false; // Email through laravel
+
+        $wgScribuntoDefaultEngine = 'luastandalone';
+        $wgScribuntoEngineConf['luastandalone']['memoryLimit'] = "50M";
+
+        $wgDebugLogFile = '/tmp/mediawiki-debug.log';
+        $wgDebugLogGroups['PluggableAuth'] = '/tmp/pluggableauth-debug.log';
+        $wgShowExceptionDetails = true;
       '';
+
+      # 'email' => 'email',
+      # 'realname' => 'name',
+      extensions = {
+        # Extensions included within the mediawiki install are enabled by setting to null here
+        PluggableAuth = pkgs.fetchzip {
+          url = "https://extdist.wmflabs.org/dist/extensions/PluggableAuth-REL1_45-2db3fff.tar.gz";
+          sha256 = "sha256-G0Vs1QXusok6WS6HMib/FX/sCMuaQd0kJKsMCKI44Kg=";
+        };
+        OpenIDConnect = pkgs.fetchzip {
+          url = "https://extdist.wmflabs.org/dist/extensions/OpenIDConnect-REL1_45-efcadd5.tar.gz";
+          sha256 = "sha256-EPKBL4CvlACLrgOPyMfv0onGuSnjilZyx6ci6jh2fuw=";
+        };
+        Scribunto = null;
+        TemplateStyles = null;
+      };
     };
   };
 }
