1394fc7e2f9660f9837b1502ccf01a9eb32a7a97
Michael Manning <michael@manning390.com>
2026-09-07 19:04:16 -0700

    Added homelab caddy cert

--- dendritic/features/homelab/caddy-local-ca/default.nix
+++ dendritic/features/homelab/caddy-local-ca/default.nix
@@ -2,7 +2,7 @@
   flake.aspects.caddy-local-ca = {
     description = "Trust homelab's Caddy internal certificate authority";
     nixos = {
-      security.pki.certificateFiles = [ ./glaciem-homelab-root-ca.crt ];
+      security.pki.certificateFiles = [./glaciem-homelab-root-ca.crt];
     };
   };
 }
